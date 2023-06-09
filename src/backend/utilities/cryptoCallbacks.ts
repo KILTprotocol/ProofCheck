3b249c452536b4224d3e8da9cb1aaf8d0c01cdbc
Tom Adler <me@arty.name>
2023-06-09 13:30:30 +0200

    upgrade SDK and others: less nacl*

--- src/backend/utilities/cryptoCallbacks.ts
+++ src/backend/utilities/cryptoCallbacks.ts
@@ -1,8 +1,8 @@
-import { naclOpen, naclSeal } from '@polkadot/util-crypto';
 import {
   DecryptCallback,
   DidResourceUri,
   EncryptCallback,
+  Utils,
 } from '@kiltprotocol/sdk-js';
 
 import { keypairsPromise } from './keypairs';
@@ -30,37 +30,36 @@ export async function encrypt({
   const { did, keyAgreementKey } = await didDocumentPromise;
   const keyUri: DidResourceUri = `${did}${keyAgreementKey.id}`;
 
-  const { sealed, nonce } = naclSeal(
+  const { box, nonce } = Utils.Crypto.encryptAsymmetric(
     data,
-    keyAgreement.secretKey,
     peerPublicKey,
+    keyAgreement.secretKey,
   );
 
   return {
-    data: sealed,
+    data: box,
     nonce,
     keyUri,
   };
 }
 
 export async function decrypt({
-  data,
+  data: box,
   nonce,
   peerPublicKey,
 }: Parameters<DecryptCallback>[0]) {
   const { keyAgreement } = await keypairsPromise;
 
-  const decrypted = naclOpen(
-    data,
-    nonce,
+  const data = Utils.Crypto.decryptAsymmetric(
+    { box, nonce },
     peerPublicKey,
     keyAgreement.secretKey,
   );
-  if (!decrypted) {
+  if (!data) {
     throw new Error('Failed to decrypt with given key');
   }
 
   return {
-    data: decrypted,
+    data,
   };
 }
