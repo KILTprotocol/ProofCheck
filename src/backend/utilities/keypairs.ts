6cee93cce86baa939106b9ceca00b6b5e64f4eb0
dudleyneedham <dudleyneedham1993@gmail.com>
2023-08-18 12:01:06 +0200

    feat: unification of keypairs

--- src/backend/utilities/keypairs.ts
+++ src/backend/utilities/keypairs.ts
@@ -3,17 +3,24 @@ import { cryptoWaitReady } from '@polkadot/util-crypto';
 
 import { configuration } from './configuration';
 
+const signingKeyPairType = 'sr25519';
+
 export const keypairsPromise = (async () => {
   await cryptoWaitReady();
 
-  const payer = Utils.Crypto.makeKeypairFromUri(configuration.payerMnemonic);
+  const payer = Utils.Crypto.makeKeypairFromUri(
+    configuration.payerMnemonic,
+    signingKeyPairType,
+  );
 
   const authentication = Utils.Crypto.makeKeypairFromUri(
     configuration.authenticationMnemonic,
+    signingKeyPairType,
   );
 
   const assertionMethod = Utils.Crypto.makeKeypairFromUri(
     configuration.assertionMethodMnemonic,
+    signingKeyPairType,
   );
 
   const keyAgreement = Utils.Crypto.makeEncryptionKeypairFromSeed(
